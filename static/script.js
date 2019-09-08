e068e2bd52cbfe2c971a550a99fca35d4bde2bc8
Sanderzhanni <hannis@tlu.ee>
2019-09-08 20:54:38 +0300

    Final commit

--- static/script.js
+++ static/script.js
@@ -1,3 +1,9 @@
-if( window.location.href.indexOf('http://localhost/shop/product') !== -1 ) {
-   alert('Hello world');
-}
+
+const x = window.location;
+const urlParams = new URLSearchParams(window.location.search);
+const title = urlParams.get("title");
+const cost = urlParams.get("cost");
+const src = urlParams.get("src");
+
+console.log(title, cost, src);
+alert("Title: " + title + " Cost: " + cost + " src= " + src);
