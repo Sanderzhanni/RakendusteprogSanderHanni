ebef15448fb5ed6201aafb3475cab2f6503c5fd4
Sanderzhanni <hannis@tlu.ee>
2019-09-16 14:28:35 +0300

    jsx

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+/*jshint esversion: 8 */
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.getElementById("app");
+
+ReactDOM.render(
+  <button>Hello World</button>,
+  root
+);
