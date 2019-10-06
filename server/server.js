bc1d4865c781e7def8dfbda86322443c6971a97a
Sanderzhanni <hannis@tlu.ee>
2019-10-06 22:32:05 +0300

    Mongo DB

--- server/server.js
+++ server/server.js
@@ -3,7 +3,35 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 const path = require("path");
 const DB = require("./database.js");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+var kittySchema = new mongoose.Schema({
+    name: String
+});
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+const kitten1 = new Kitten({
+    name: "catname1"
+});
+
+const DB_URL = `mongodb+srv://` + process.env.DB_USERNAME + `:` + process.env.DB_PASS + `@rakprog-aq8p2.mongodb.net/` + process.env.DB_NAME + `?retryWrites=true&w=majority`;
+
+mongoose.connect(DB_URL)
+    .then(() => {
+        console.log("Database access success");
+        kitten1.save(err => {
+            if (err) {
+                console.log("Error occured! ", err);
+            } else {
+                console.log("Successful save!");
+            }
+        })
+    })
+    .catch((err) => {
+        console.log("error: ", err);
+    });
 
 app.get("/api/items", (req, res) => {
     res.json(DB.getItems());
