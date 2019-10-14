3ef4699c9dfe2ec625e97864ab6c5dce2a3b3987
Sanderzhanni <hannis@tlu.ee>
2019-10-14 13:04:59 +0300

    build

--- src/components/SortDropdown.jsx
+++ src/components/SortDropdown.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./SelectMenu.css";
+import "./selectMenu.css";
 
 const SortDropdown = ({ direction, onChange }) => {
     return (
